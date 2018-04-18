dcce755b2ebc395b3e18d45401fb74ddf11efc82
arpitkansal <16.arpitkansal@gmail.com>
2018-04-18 19:39:02 +0530

    Initial commit

---
+++ app/src/main/java/com/navigation/android/gitpractice/MainActivity.java
@@ -0,0 +1,17 @@
+package com.navigation.android.gitpractice;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Super Code goes here
+        //Hope you will get it :)
+    }
+}
